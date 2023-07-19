c469ec8e9c44427b4604edb203b0e5b80b82d25a
Dhruv T <dthota8@gmail.com>
2023-07-19 12:44:34 -0400

    ready for assessment

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def main():
     # print(pb2.max_values([-5, -2, -1, -11])) # -> [1, 2]  
 
     # Problem #3: Youngest Student
-    students = {"Alice": 18, "Bob": 20, "Charlie": 19, "David": 22, "Jay": 20}
-    # print(pb3.youngest_student(students))  # Expected output: "Alice"
+    students1 = {"Alice": 18, "Bob": 20, "Charlie": 19, "David": 22, "Jay": 20}
+    students2 = {"Bob": 20, "Charlie": 19, "David": 22, "Jay": 20, "Alice": 18, "Drake": 16, "Mary": 25}
+    # print(pb3.youngest_student(students1))  # Expected output: "Alice"
+    # print(pb3.youngest_student(students2))  # Expected output: "Drake"
 
 
 if __name__ == '__main__':
--- problems/pb3.py
+++ problems/pb3.py
@@ -2,5 +2,10 @@
 def youngest_student(students):
     pass
 
-# students = {"Drake": 21, "Alice": 18, "Bob": 20, "Charlie": 19, "David": 22, "Jay": 20}
-# print(youngest_student(students))  # Expected output: "Alice"
+
+
+
+students1 = {"Alice": 18, "Bob": 20, "Charlie": 19, "David": 22, "Jay": 20}
+students2 = {"Bob": 20, "Charlie": 19, "David": 22, "Jay": 20, "Alice": 18, "Drake": 16, "Mary": 25}
+# print(pb3.youngest_student(students1))  # Expected output: "Alice"
+# print(pb3.youngest_student(students2))  # Expected output: "Drake"
